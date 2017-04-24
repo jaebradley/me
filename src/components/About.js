a8eaac927dd9580c9d7ed7f05a868ee1405b7640
jaebradley <jae.b.bradley@gmail.com>
2017-04-23 21:11:19 -0400

    update about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { PageHeader, Grid, Row, Col } from 'react-bootstrap';
+import { PageHeader, Grid, Row, Col, ListGroup, ListGroupItem, Image } from 'react-bootstrap';
 import ContactBar from './ContactBar';
+import JaeImage from '../images/jae.png';
 
 class About extends Component {
     render = () => (
@@ -11,6 +12,33 @@ class About extends Component {
             <PageHeader className="page-header">
               About
             </PageHeader>
+          </Col>
+          <Col xsHidden md={4} />
+        </Row>
+        <Row>
+          <Col xsHidden md={4} />
+          <Col xs={6} md={4}>
+            <Image src={JaeImage} circle className="about-image"/>
+          </Col>
+          <Col xsHidden md={4} />
+        </Row>
+        <Row>
+          <Col xsHidden md={4} />
+          <Col xs={6} md={4}>
+            <ListGroup className="about-description">
+              <ListGroupItem>
+                Bowdoin College graduate
+              </ListGroupItem>
+              <ListGroupItem>
+                Boston-based backend software engineer.
+              </ListGroupItem>
+              <ListGroupItem>
+                Avid sports fan (particularly the NBA).
+              </ListGroupItem>
+              <ListGroupItem>
+                Once finished eleven soups at Olive Garden's Unlimited Soup, Salads, and Breadsticks Lunch Deal.
+              </ListGroupItem>
+            </ListGroup>
           </Col>
           <Col xsHidden md={4} />
         </Row>
--- src/components/ContactBar.js
+++ src/components/ContactBar.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Thumbnail } from 'react-bootstrap';
 
 import GithHubImage from '../images/github.png';
 import GmailImage from '../images/gmail.png';
 import LinkedInImage from '../images/linkedin.png';
 import TwitterImage from '../images/twitter.png';
-
-import ContactIcon from './ContactIcon';
 
 class ContactBar extends Component {
   render = () => (
@@ -14,19 +12,31 @@ class ContactBar extends Component {
       <Row className="show-grid">
         <Col xsHidden md={4} />
         <Col xs={6} md={4} className="contact-bar">
-          <ContactIcon href={"https://github.com/jaebradley"} src={GithHubImage} alt="Github" />
-          <ContactIcon
-            href={"https://www.linkedin.com/in/jaebradley/"}
-            src={LinkedInImage}
-            alt="LinkedIn" />
-          <ContactIcon
-            href={"mailto:jae.b.bradley@gmail.com"}
-            src={GmailImage}
-            alt="Gmail" />
-          <ContactIcon
-            href={"https://twitter.com/jaebradley"}
-            src={TwitterImage}
-            alt="Twitter" />
+          <Col xs={12} md={3}>
+            <Thumbnail
+              target="_blank" href="https://github.com/jaebradley" src={GithHubImage} alt="Github" />
+          </Col>
+          <Col xs={12} md={3}>
+            <Thumbnail
+              target="_blank"
+              href="https://www.linkedin.com/in/jaebradley/"
+              src={LinkedInImage}
+              alt="LinkedIn" />
+          </Col>
+          <Col xs={12} md={3}>
+            <Thumbnail
+              target="_blank"
+              href="mailto:jae.b.bradley@gmail.com"
+              src={GmailImage}
+              alt="Gmail" />
+          </Col>
+          <Col xs={12} md={3}>
+            <Thumbnail
+              target="_blank"
+              href="https://twitter.com/jaebradley"
+              src={TwitterImage}
+              alt="Twitter" />
+          </Col>
         </Col>
         <Col xsHidden md={4} />
       </Row>
