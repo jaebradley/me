657736c0971bcba45f23b31d87bec4055b251368
jaebradley <jae.b.bradley@gmail.com>
2017-04-26 00:10:15 -0400

    update menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { push as BurgerMenu } from 'react-burger-menu'
+import { push as BurgerMenu } from 'react-burger-menu';
+import { PageHeader, Image, Grid, Row, Col } from 'react-bootstrap';
+import MdPerson from 'react-icons/lib/md/person';
+import GoFileDirectory from 'react-icons/lib/go/file-directory';
+import JaeImage from '../images/jae.png';
 
 class Menu extends Component {
   render = () => (
     <BurgerMenu pageWrapId={this.props.pageWrapId} outerContainerId={this.props.outerContainerId}>
-      <a id="projects" className="menu-item" href="/projects">Projects</a>
-      <a id="about" className="menu-item" href="/about">About</a>
+      <PageHeader>
+        <Image src={JaeImage} circle className="about-image"/>
+      </PageHeader>
+      <div>
+        <MdPerson className="menu-icon"/><a id="about" className="menu-item" href="/about">About</a>
+      </div>
+      <div>
+        <GoFileDirectory className="menu-icon"/><a id="projects" className="menu-item" href="/projects">Projects</a>
+      </div>
     </BurgerMenu>
   );
 }
